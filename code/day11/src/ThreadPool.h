937ca104405e0fa4defdfa4e8ac9b4d9de0af256
yuesong-feng <yuesong-feng@foxmail.com>
2021-12-30 14:25:48 +0800

    update

--- code/day11/src/ThreadPool.h
+++ code/day11/src/ThreadPool.h
@@ -33,11 +33,9 @@ public:
 };
 
 
-//不能放在cpp文件，原因不明
+//不能放在cpp文件，原因是C++编译器不支持模版的分离编译
 template<class F, class... Args>
-auto ThreadPool::add(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
-{
+auto ThreadPool::add(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
     using return_type = typename std::result_of<F(Args...)>::type;
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
