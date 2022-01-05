7c0f3c5f2da38404f2f33a4c8c6062a939da1d54
hjhj111 <2268716068@qq.com>
2022-01-05 19:09:12 +0800

    Update client.cpp

--- code/day01/client.cpp
+++ code/day01/client.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-04 20:03:45
+ * @LastEditTime: 2022-01-05 19:08:58
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \30dayMakeCppServer\code\day01\client.cpp
+ */
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
@@ -11,7 +19,7 @@ int main() {
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8888);
 
-    bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+    //bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)); 客户端不进行bind操作
 
     connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));    
     
